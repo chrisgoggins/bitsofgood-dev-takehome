3f0319e0fcb307ea6999528a495c6ec489034d8e
chrisgoggins <55658109+chrisgoggins@users.noreply.github.com>
2025-08-22 11:09:11 -0400

    feat: add cool page

--- src/app/cool/page.tsx
+++ src/app/cool/page.tsx
@@ -1,13 +1,15 @@
 export default function Kewl() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-primary text-white gap-5">
-      {/* 
-      Write something unique about you here! 
-      It could be a club you're part of, a weird skill you have, or something special that happened to you.
-      Feel free to put links, images, whatever! 
-      Don't worry about styling- we aren't grading you on this- it's just to get to know you better! :) 
-      */}
-      😎😎😎
+    <div className="min-h-screen flex flex-col justify-center items-center bg-primary text-white text-center gap-4 p-6">
+      <p>- I have been 110 ft under the ocean while SCUBA diving (with a Caribbean reef shark as well)</p>
+      <p>- I have been on a US Navy nuclear attack submarine (SSN 779)</p>
+      <p>- My favorite drink is milk (by far)</p>
+
+      <p>Those are the three most random and unrelated unique facts that I could think of</p>
+      <p>Other than that, I am Chris Goggins, a second-year CS Major (Intel & SysArch) at Tech</p>
+      <p>I am involved in GT WebDev, GT Swim Club, and I am a UTA for CS 1331</p>
+      <p>I am passionate about pursuing mission-driven opportunities where my work will have a *real* impact</p>
+      <p>Links: <a className="underline" href="https://chrisgoggins.com" target="_blank">Personal Website</a></p>
     </div>
   );
 }
